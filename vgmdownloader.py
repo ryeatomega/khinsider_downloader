1688fee067b67b097df7be96ee8935af6920490f
Rye <lepyrogang@proton.me>
2025-09-25 19:35:13 +0330

    fix: refactoring feat: rewrote LICENSE

--- vgmdownloader.py
+++ vgmdownloader.py
@@ -11,17 +11,17 @@ session.headers["User-Agent"] = (
 download_links = []
 
 
-def dwnfile(name: str):
+def dwnfile(link):
     """Function to download the music file
 
     Args:
-        name (str): the link of the file to be downloaded, also used to determine filename
+        link : the link of the file to be downloaded, also used to determine filename
     """
-    filename = unquote(name.split("/")[6])
+    filename = unquote(link.split("/")[6])
     match input(f"Do you want to download |{filename}| y/n/q?\t").lower():
         case "y":
             print(f"Downloading {filename}")
-            response = session.get(name, stream=True)
+            response = session.get(link, stream=True)
             response.raise_for_status()
             with open(filename, "wb") as f:
                 for chunk in response.iter_content(chunk_size=8192):
@@ -33,20 +33,19 @@ def dwnfile(name: str):
             exit(0)
         case _:
             print("Illegal Key Pressed. retry.")
-            dwnfile(name)
+            dwnfile(link)
 
 
 def main():
     """Main function. it will scrape the initial page and get the list of songs, and go through each one and uses dwnfile() to download them
     """
-    init_page = session.get(initial_page)
-    init_soup = BeautifulSoup(init_page.text, "html.parser")
+    init_html = session.get(initial_page).text
+    init_soup = BeautifulSoup(init_html, "html.parser")
     init_td_tags = init_soup.find_all("td", class_="playlistDownloadSong")
     init_a_tags = [a["href"] for td in init_td_tags for a in td.find_all("a")]
 
     for link in init_a_tags:
-        down_page = session.get(urljoin("https://downloads.khinsider.com/", link))
-        down_html = down_page.text
+        down_html = session.get(urljoin("https://downloads.khinsider.com/", link)).text
         down_soup = BeautifulSoup(down_html, "html.parser")
         for a in down_soup.find_all("a", href=True):
             if a["href"].endswith(".flac"):

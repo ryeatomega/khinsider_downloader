6b80ab3e2de3c00064ffe614d409834bb29675da
Rye <lepyrogang@proton.me>
2025-09-25 21:07:43 +0330

    feat: updated code to use asyncio and aiohttp instead of requests

--- vgmdownloader.py
+++ vgmdownloader.py
@@ -1,56 +1,77 @@
-import requests
+import asyncio
+import aiohttp
 from bs4 import BeautifulSoup
 from urllib.parse import unquote, urljoin
 
+# This is the soundtrack you're gonna download, you should go to their website and search for the OST you want.
 initial_page = "https://downloads.khinsider.com/game-soundtracks/album/risk-of-rain-2-survivors-of-the-void-2022"
-session = requests.Session()
-session.headers["User-Agent"] = (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
-)
-
 download_links = []
 
 
-def dwnfile(link):
+async def fetch_html(session: aiohttp.ClientSession, link: str) -> str:
+    """Fetches the HTML content of the link
+
+    Args:
+        session (aiohttp.ClientSession): your aiohttp session
+        link (str): the link to grab the html content from
+
+    Returns:
+        _type_: html content, as string
+    """
+    async with session.get(link) as response:
+        return await response.text()
+
+
+async def dwnfile(session: aiohttp.ClientSession, link: str):
     """Function to download the music file
 
     Args:
         link : the link of the file to be downloaded, also used to determine filename
     """
     filename = unquote(link.split("/")[6])
-    match input(f"Do you want to download |{filename}| y/n/q?\t").lower():
+    match input(f"Do you want to download |{filename}| y/n/q? ").lower():
         case "y":
             print(f"Downloading {filename}")
-            response = session.get(link, stream=True)
-            response.raise_for_status()
+            file = await fetch_html(session, link)
+            file.raise_for_status()
             with open(filename, "wb") as f:
-                for chunk in response.iter_content(chunk_size=8192):
+                async for chunk in file.content.iter_chunked(8192):
                     f.write(chunk)
                 print("Download completed")
         case "n":
             return
         case "q":
-            exit(0)
+            raise asyncio.CancelledError()
         case _:
             print("Illegal Key Pressed. retry.")
-            dwnfile(link)
+            await dwnfile(link)
 
 
-def main():
-    """Main function. it will scrape the initial page and get the list of songs, and go through each one and uses dwnfile() to download them
-    """
-    init_html = session.get(initial_page).text
-    init_soup = BeautifulSoup(init_html, "html.parser")
-    init_td_tags = init_soup.find_all("td", class_="playlistDownloadSong")
-    init_a_tags = [a["href"] for td in init_td_tags for a in td.find_all("a")]
+async def main():
+    """Main function. it will scrape the initial page and get the list of songs, and go through each one and uses dwnfile() to download them"""
+    async with aiohttp.ClientSession(
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+        }
+    ) as global_session:
+        try:
+            init_html = await fetch_html(global_session, initial_page)
+            init_soup = BeautifulSoup(init_html, "html.parser")
+            init_td_tags = init_soup.find_all("td", class_="playlistDownloadSong")
+            init_a_tags = [a["href"] for td in init_td_tags for a in td.find_all("a")]
 
-    for link in init_a_tags:
-        down_html = session.get(urljoin("https://downloads.khinsider.com/", link)).text
-        down_soup = BeautifulSoup(down_html, "html.parser")
-        for a in down_soup.find_all("a", href=True):
-            if a["href"].endswith(".flac"):
-                dwnfile(a["href"])
+            for link in init_a_tags:
+                down_html = await fetch_html(
+                    global_session, urljoin("https://downloads.khinsider.com/", link)
+                )
+                down_soup = BeautifulSoup(down_html, "html.parser")
+
+                for a in down_soup.find_all("a", href=True):
+                    if a["href"].endswith(".flac"):
+                        await dwnfile(global_session, a["href"])
+        except asyncio.CancelledError:
+            print("Quitting gracefully.")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())

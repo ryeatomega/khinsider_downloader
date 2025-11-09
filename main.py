fc4fb23fb398d17adb28d71d22fae84f8d6ba890
Rye <lepyrogang@proton.me>
2025-11-09 20:16:08 +0330

    update: more refactoring, but it still doesn't work

--- main.py
+++ main.py
@@ -1,17 +1,8 @@
 import asyncio
 import aiohttp
-from bs4 import BeautifulSoup, ResultSet, Tag
+import sys
+from bs4 import BeautifulSoup, ResultSet
 from urllib.parse import unquote, urljoin
-
-# This is the soundtrack you're gonna download, you should go to their website and search for the OST you want.
-ost_link = "https://downloads.khinsider.com/game-soundtracks/album/risk-of-rain-2-survivors-of-the-void-2022"
-
-
-async def find_flac_link(tags: ResultSet[Tag]) -> str:
-    for tag in tags:
-        href = tag["href"]
-        if href.endswith(".flac"):
-            return href
 
 
 async def fetch_html(session: aiohttp.ClientSession, link: str):
@@ -19,71 +10,85 @@ async def fetch_html(session: aiohttp.ClientSession, link: str):
         return BeautifulSoup(await response.text(), "lxml")
 
 
-async def init_page_scrape(init_link: str) -> list[str]:
-    async with aiohttp.ClientSession(
-        headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
-        }
-    ) as global_session:
-        soup = await fetch_html(global_session, init_link)
-        td_tags = soup.select('td.playlistDownloadSong a[href*=".mp3"]')
+async def init_page_scrape(client: aiohttp.ClientSession, init_link: str) -> list[str]:
+    print("[INFO] Getting the album page.")
+    async with client as global_session:
+        soup: BeautifulSoup = await fetch_html(global_session, init_link)
+        td_tags: ResultSet = soup.select('td.playlistDownloadSong a[href*=".mp3"]')
         a_tags = [
-            urljoin("https://downloads.khinsider.com/", a_tag["href"])
-            for a_tag in td_tags
+            urljoin("https://downloads.khinsider.com/", td_tag["href"])
+            for td_tag in td_tags
         ]
         print(a_tags)
-
         return a_tags
 
 
-async def down_page_scrape(init_content: list[str]) -> list[str]:
-    download_links = []
-    async with aiohttp.ClientSession(
-        headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
-        }
-    ) as global_session:
+async def down_page_scrape(
+    client: aiohttp.ClientSession,
+    init_content: list[str],
+) -> list[str]:
+    print("[INFO] Getting track links.")
+    async with client as global_session:
 
         async def process(link):
             down_soup = await fetch_html(global_session, link)
             hrefs = down_soup.select("a:has(span.songDownloadLink)")
-            return await find_flac_link(hrefs)
+            a = next(
+                (tag["href"] for tag in hrefs if tag["href"].endswith(".mp3")), None
+            )
+            return a
 
         async with asyncio.TaskGroup() as tg:
-            tasks = [tg.create_task(process(link)) for link in init_content]
+            tasks: list = [tg.create_task(process(link)) for link in init_content]
 
-    download_links = [task.result() for task in tasks]
+    download_links: list[str] = [task.result() for task in tasks]
     return download_links
 
 
 async def download_list(download_links: list[str]) -> None:
+    print("[INFO] Downloading files.")
     for link in download_links:
         filename = unquote(link.split("/")[6])
-        match input(f"Do you want to download |{filename}| y/n/q? ").lower():
-            case "y":
-                print(f"Downloading {filename}")
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(link) as file:
-                        file.raise_for_status()
-                        with open(filename, "wb") as f:
-                            async for chunk in file.content.iter_chunked(8192):
-                                f.write(chunk)
-                            print("Download completed")
-            case "n":
-                continue
-            case "q":
-                raise asyncio.CancelledError()
-            case _:
-                print("Illegal Key Pressed. retry.")
-                await download_list()
+        while True:
+            match input(f"[INFO] Download {filename} [y/n/q]?\n").lower():
+                case "y":
+                    print(f"[INFO] Downloading {filename}.")
+                    async with aiohttp.ClientSession() as session:
+                        async with session.get(link) as file:
+                            file.raise_for_status()
+                            with open(filename, "wb") as f:
+                                async for chunk in file.content.iter_chunked(8192):
+                                    f.write(chunk)
+                                print(f"[INFO] Download {filename} completed.")
+                    break
+                case "n":
+                    break
+                case "q":
+                    raise asyncio.CancelledError()
+                case _:
+                    print("[ERROR] Invalid option! Try again!", file=sys.stderr)
+
+
+async def main():
+    if len(sys.argv) < 2:
+        print("[ERROR] Album link not specified.", file=sys.stderr)
+        print("[USAGE] main.py <url>")
+    else:
+        async with aiohttp.ClientSession(
+            headers={
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+            }
+        ) as client:
+            init_page_content: list[str] = await init_page_scrape(client, sys.argv[1])
+            down_page_content: list[str] = await down_page_scrape(
+                client, init_page_content
+            )
+        try:
+            asyncio.run(download_list(down_page_content))
+        except asyncio.CancelledError:
+            print("[INFO] Quitting gracefully.")
+            exit(0)
 
 
 if __name__ == "__main__":
-    init_page_content: list[str] = asyncio.run(init_page_scrape(ost_link))
-    down_page_content: list[str] = asyncio.run(down_page_scrape(init_page_content))
-    print(down_page_content)
-    try:
-        asyncio.run(download_list(down_page_content))
-    except asyncio.CancelledError:
-        print("Quitting gracefully.")
-        exit(0)
+    asyncio.run(main())

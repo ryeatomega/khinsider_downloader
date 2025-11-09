b4af0ccc11db5a4e22581d8c679be8ca3e331b37
Rye <lepyrogang@proton.me>
2025-11-09 19:10:48 +0330

    fix: using css selectors and also renamed file to main.py

--- main.py
+++ main.py
@@ -19,6 +19,45 @@ async def fetch_html(session: aiohttp.ClientSession, link: str) -> str:
         return await response.text()
 
 
+async def init_page_scrape(init_link: str) -> list[str]:
+    async with aiohttp.ClientSession(
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+        }
+    ) as global_session:
+        init_html = await fetch_html(global_session, init_link)
+        soup = BeautifulSoup(init_html, "lxml")
+        td_tags = soup.select('td.playlistDownloadSong a[href*=".mp3"]')
+        a_tags = [
+            urljoin("https://downloads.khinsider.com/", a_tag["href"])
+            for a_tag in td_tags
+        ]
+        print(a_tags)
+
+        return a_tags
+
+
+async def down_page_scrape(init_content: list[str]) -> list[str]:
+    download_links = []
+    async with aiohttp.ClientSession(
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
+        }
+    ) as global_session:
+
+        async def process(link):
+            down_text = await fetch_html(global_session, link)
+            down_soup = BeautifulSoup(down_text, "lxml")
+            hrefs = down_soup.select("a:has(span.songDownloadLink)")
+            return await find_flac_link(hrefs)
+
+        async with asyncio.TaskGroup() as tg:
+            tasks = [tg.create_task(process(link)) for link in init_content]
+
+    download_links = [task.result() for task in tasks]
+    return download_links
+
+
 async def download_list(download_links: list[str]) -> None:
     for link in download_links:
         filename = unquote(link.split("/")[6])
@@ -39,45 +78,6 @@ async def download_list(download_links: list[str]) -> None:
             case _:
                 print("Illegal Key Pressed. retry.")
                 await download_list()
-
-
-async def down_page_scrape(init_content: list[str]) -> list[str]:
-    download_links = []
-    async with aiohttp.ClientSession(
-        headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
-        }
-    ) as global_session:
-
-        async def process(link):
-            down_text = await fetch_html(global_session, link)
-            down_soup = BeautifulSoup(down_text, "lxml")
-            hrefs = down_soup.find_all("a", href=True)
-            return await find_flac_link(hrefs)
-
-        async with asyncio.TaskGroup() as tg:
-            tasks = [tg.create_task(process(link)) for link in init_content]
-
-    download_links = [task.result() for task in tasks]
-    return download_links
-
-
-async def init_page_scrape(init_link: str) -> list[str]:
-    async with aiohttp.ClientSession(
-        headers={
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36"
-        }
-    ) as global_session:
-        init_html = await fetch_html(global_session, init_link)
-        soup = BeautifulSoup(init_html, "lxml")
-        td_tags = soup.find_all("td", class_="playlistDownloadSong")
-        a_tags = [
-            urljoin("https://downloads.khinsider.com/", a["href"])
-            for td in td_tags
-            for a in td.find_all("a", href=True)
-        ]
-
-        return a_tags
 
 
 if __name__ == "__main__":
